b513c3548728fefa34f1ba989d418dccb552fc41
dougsneri <dsds.douglas@gmail.com>
2020-03-22 00:17:15 -0300

    first commit

---
+++ src/main/java/br/com/dougg/growdiary/GrowdiaryApplication.java
@@ -0,0 +1,13 @@
+package br.com.dougg.growdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowdiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowdiaryApplication.class, args);
+	}
+
+}
